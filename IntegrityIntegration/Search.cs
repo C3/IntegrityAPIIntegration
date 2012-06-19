e3812d068303e309f62865d90f7a4a290c1613f0
Sean Caffery <sean.caffery@c3businesssolutions.com>
2012-06-19 16:03:59 +1000

    add initial search class with condition builder

---
+++ IntegrityIntegration/Search.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace IntegrityIntegration
+{
+    class Search
+    {
+        IntegrityDataset _dataset;
+        List<string> _conditions = new List<string>();
+        public Search(IntegrityDataset dataset)
+        {
+            _dataset = dataset;
+        }
+
+        public List<string> Conditions
+        {
+            get { return _conditions; }
+        }
+
+        public string[] Perform(){
+            string[] results = new string[0];
+            return results;
+        }
+
+
+        internal void AddCondition(string columnName, string value)
+        {
+            _conditions.Add(string.Format("[{0}][{1}][exactly][]={2}", _dataset.m_tableName, columnName, value));
+        }
+    }
+}
---
+++ IntegrityIntegration/tests/SearchTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using IntegrityIntegration;
+
+namespace IntegrityAPITests
+{
+
+    [TestFixture()]
+    public class SearchTests
+    {
+
+        [Test()]
+        public void SearchWithNoResults(){
+            IntegrityDataset ds = new IntegrityDataset();
+            Search search = new Search(ds);
+            string[] results = new string[0];
+            Assert.AreEqual(search.Perform(), results);
+        }
+
+        [Test()]
+        public void SearchWithResults()
+        {
+            IntegrityDataset ds = new IntegrityDataset();
+            Search search = new Search(ds);
+            string[] results = {"some result"};
+
+            Assert.AreEqual(search.Perform(), results);
+        }
+
+        [Test()]
+        public void AddCondition()
+        {
+            IntegrityDataset ds = new IntegrityDataset();
+            ds.m_tableName = "people";
+            Search search = new Search(ds);
+            string condition = "[people][name][exactly][]=bob";
+            string columnName, value;
+            columnName = "name";
+            value = "bob";
+
+            List<string> conditions = new List<string> { condition };
+            search.AddCondition(columnName, value);
+            Assert.AreEqual(search.Conditions, conditions);
+        }
+
+    }
+
+
+}
