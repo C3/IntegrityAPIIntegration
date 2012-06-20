20a58cb05c583dde05de9787cac391e560911be7
Sean Caffery <sean.caffery@c3businesssolutions.com>
2012-06-20 10:36:56 +1000

    add pagination parameters, audit id parameter

--- IntegrityIntegration/Search.cs
+++ IntegrityIntegration/Search.cs
@@ -8,10 +8,12 @@ namespace IntegrityIntegration
 {
     class Search
     {
+        const string PER_PAGE = "500";
         IntegrityDataset _dataset;
         List<string> _conditions = new List<string>();
         public Search(IntegrityDataset dataset)
         {
+            AuditId = 0;
             _dataset = dataset;
         }
 
@@ -20,15 +22,30 @@ namespace IntegrityIntegration
             get { return _conditions; }
         }
 
+        public int AuditId
+        {
+            get;
+            set;
+        }
+
         public string[] Perform(){
             string[] results = new string[0];
             return results;
         }
 
-
         internal void AddCondition(string columnName, string value)
         {
             _conditions.Add(string.Format("[{0}][{1}][exactly][]={2}", _dataset.m_tableName, columnName, value));
+        }
+
+        internal string PaginationParams(int pageNumber)
+        {
+            return string.Format("page={0}&per_page={1}", pageNumber, PER_PAGE);
+        }
+
+        internal string AuditIdParam()
+        {
+            return string.Format("audit_id_gt={0}", AuditId);
         }
 
         public string ToQueryConditions()
@@ -46,6 +63,8 @@ namespace IntegrityIntegration
                 query.Append(condition);
                 query.Append("&");
             }
+
+            query.Append(AuditIdParam());
 
             return query.ToString();
         }
--- IntegrityIntegration/tests/SearchTests.cs
+++ IntegrityIntegration/tests/SearchTests.cs
@@ -11,6 +11,12 @@ namespace IntegrityAPITests
     [TestFixture()]
     public class SearchTests
     {
+        string _audit_id_param;
+        [SetUp()]
+        public void TestSetup()
+        {
+            _audit_id_param = "audit_id_gt=0";
+        }
 
         [Test()]
         public void SearchWithNoResults(){
@@ -55,6 +61,7 @@ namespace IntegrityAPITests
             search.AddCondition("name", "bob");
 
             string query = "[people][name][exactly][]=bob&";
+            query += _audit_id_param;
             Assert.AreEqual(query, search.ToQueryConditions());
         }
 
@@ -69,6 +76,7 @@ namespace IntegrityAPITests
 
             string query = "[people][name][exactly][]=bob&";
             query += "[people][name][exactly][]=sally&";
+            query += _audit_id_param;
             Assert.AreEqual(query, search.ToQueryConditions());
         }
 
@@ -84,6 +92,7 @@ namespace IntegrityAPITests
             string query = "upload_qualifiers[0][dataset_attribute_name]=name";
             query += "&upload_qualifiers[0][qualifier_values][]=bob";
             query += "&upload_qualifiers[0][qualifier_values][]=sally&";
+            query += _audit_id_param;
 
             Assert.AreEqual(query, search.ToQueryConditions());
         }
@@ -104,8 +113,30 @@ namespace IntegrityAPITests
             query += "&upload_qualifiers[0][qualifier_values][]=bob";
             query += "&upload_qualifiers[1][dataset_attribute_name]=state";
             query += "&upload_qualifiers[1][qualifier_values][]=VIC&";
+            query += _audit_id_param;
 
             Assert.AreEqual(query, search.ToQueryConditions());
+        }
+
+        [Test()]
+        public void SearchWithAuditIdParam()
+        {
+            IntegrityDataset ds = new IntegrityDataset();
+            Search search = new Search(ds);
+            search.AuditId = 100;
+
+            string query = "audit_id_gt=100";
+
+            Assert.AreEqual(query, search.ToQueryConditions());
+        }
+
+        [Test()]
+        public void PaginationParameters()
+        {
+            IntegrityDataset ds = new IntegrityDataset();
+            Search search = new Search(ds);
+
+            Assert.AreEqual("page=1&per_page=500", search.PaginationParams(1));
         }
 
     }
