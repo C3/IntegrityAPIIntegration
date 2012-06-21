cb0418e797bc5e4967a3cad0aeb3816c508d89d1
Sean Caffery <sean.caffery@c3businesssolutions.com>
2012-06-21 11:12:16 +1000

    add initial staged row representation

---
+++ IntegrityIntegration/Record.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace IntegrityIntegration
+{
+    class Record
+    {
+        Dictionary<string, string> columnValues;
+        private XmlNode recordXml;
+        public Record(XmlNode recordXml)
+        {
+            this.recordXml = recordXml;
+            columnValues = new Dictionary<string, string>(recordXml.ChildNodes.Count);
+            foreach (XmlNode column in recordXml.ChildNodes)
+            {
+                columnValues[column.Name] = column.InnerText;
+            }
+
+        }
+
+        public string this[string columnName]
+        {
+            get { return columnValues[columnName]; }
+        }
+
+    }
+}
---
+++ IntegrityIntegration/tests/RecordTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using NUnit.Framework;
+
+namespace IntegrityIntegration
+{
+    [TestFixture()]
+    public class RecordTest
+    {
+
+        [Test()]
+        public void RecordFieldAccess()
+        {
+            string resultXml = "<people><row><name>Tommy</name></row></people>";
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(resultXml);
+            XmlNode tommy = doc.SelectSingleNode("//row");
+            
+            Assert.AreEqual("Tommy", new Record(tommy)["name"]);
+        }
+    }
+}
